a172eae6d16021ddcf92837af219de79857105de
Joaco <joacobonvechi2272002@gmail.com>
2022-08-10 20:47:51 -0300

    sql terminado

--- peluqueria_canina.sql
+++ peluqueria_canina.sql
@@ -37,7 +37,7 @@ constraint fk_id_p foreign key (perro) references perro(id_perro)
 
 # INSERCIONES #
 
-
+# 2 #
 
 INSERT INTO dueno VALUES (45184238, 'Maximiliano','Tabaez',1178479981,'Jose Hernandez 1819');
 
@@ -45,12 +45,48 @@ INSERT INTO perro (nombre, fecha_de_nacimiento, sexo, dni_dueno) VALUES ('Tito',
 
 INSERT INTO dueno VALUES (44178239, 'Jose','Galindez',71482978,'Maria Korcheck 2478');
 
+INSERT INTO dueno VALUES (39876342, 'Pedro','Marquez',71489987,'Marcos de ALdaño 2876');
+
 INSERT INTO perro (nombre, fecha_de_nacimiento, sexo, dni_dueno) VALUES ('Maite', '2003-05-27' ,'F',44178239);
 INSERT INTO perro (nombre, fecha_de_nacimiento, sexo, dni_dueno) VALUES ('Paco', '2005-09-17' ,'M',44178239);
+INSERT INTO perro (nombre, fecha_de_nacimiento, sexo, dni_dueno) VALUES ('Mortadela', '2017-06-14' ,'M',39876342);
 
 INSERT INTO historial (fecha, perro, descripcion, monto) VALUES ('2020-06-27',1,'Castracion',4500);
 INSERT INTO historial (fecha, perro, descripcion, monto) VALUES ('2015-03-28',3,'Intoxicacion',6600);
-INSERT INTO historial (fecha, perro, descripcion, monto) VALUES ('2029-11-08',2,'Control',1000);
+INSERT INTO historial (fecha, perro, descripcion, monto) VALUES ('2019-11-08',2,'Control',1000);
+INSERT INTO historial (fecha, perro, descripcion, monto) VALUES ('2019-07-23',4,'Control',1000);
+
+# 3 #
+
+SELECT fecha, perro FROM historial WHERE (year(now()) - year(fecha))  > 5;  
+
+DELETE FROM historial WHERE (year(now()) - year(fecha))  > 5; 
+
+DELETE FROM perro WHERE id_perro = 3;
+
+SELECT * FROM perro;
+
+# 4 #
+
+UPDATE perro 
+SET fecha_de_nacimiento = '2005-07-13'
+WHERE id_perro = 2;
+
+SELECT * FROM perro;
+
+# 5 #
+
+SELECT p.nombre
+FROM perro as p
+JOIN dueno as d
+	ON p.dni_dueno = d.dni
+WHERE d.nombre LIKE '%Pedro%';
+
+
+
+
+
+
 
 
 
