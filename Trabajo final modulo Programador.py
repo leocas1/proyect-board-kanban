986f45b41a837d2478a2b827da7d7b04efff217e
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2022-08-12 16:49:06 -0300

    Tp final pyhton, ejercicio 3

---
+++ Trabajo final modulo Programador.py
@@ -0,0 +1,10 @@
+#Ejercicio 3
+#  Crear un lista en Python denominado “Perro” que contenga los siguientes valores:  13,  Puppy,  12/12/2012 , Macho, 123
+perro = [13, "Puppy", "12/12/2012", "Macho", 123]
+
+# Modificar fecha de nacimiento por 13/12/2012
+perro[2]="13/12/2012"
+
+# Remplazar dni por 28957346
+perro[-1]=28957346
+print ("Ejercicio 3:",perro)
