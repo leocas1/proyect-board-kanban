9d40fff4b8010ff6447ac84cba551d889c5622f1
Your Name <you@example.com>
2022-08-13 21:15:08 -0300

    act2-python

--- act_python/act2.py
+++ act_python/act2.py
@@ -1,5 +1,5 @@
 Dueño2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
 
-for i, value_array in enumerate(Dueño2_array):
+for i, value_array in enumerate(Dueño2):
     if(i not in (0,2)):
         print(value_array)
